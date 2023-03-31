028a26462e8791a1d386e6d518fb7edc20e384ac
Cynthia Anosike <anosikechioma28@gmail.com>
2023-03-31 23:30:47 +0100

    debug

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,10 @@ import urllib.request as urll
 import urllib.parse as parsy
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = parsy.urlencode(value).encode("ascii")
+    url = sys.argv[0]
+    value = {"email": sys.argv[0]}
+data = parsy.urlencode(value).encode("ascii")
 
-    req = urll.Request(url, data)
-    with urll.urlopen(req) as response:
-        print(response.read().decode("utf-8"))
+req = urll.Request(url, data)
+with urll.urlopen(req) as response:
+    print(response.read().decode("utf-8"))
